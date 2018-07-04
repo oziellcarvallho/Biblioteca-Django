dbec349168c24135b8a0b08303759518a31068bd
Deusimar <sousadeusimar2@gmail.com>
2018-07-03 22:25:56 -0300

    commit

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.contrib import messages
 
 @login_required
 def index(request):
